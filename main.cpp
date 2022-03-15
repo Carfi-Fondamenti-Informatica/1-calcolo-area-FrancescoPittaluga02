44bce07887cbb1e138d729b8046bfdaff5eab263
FrancescoPittaluga02 <100779816+FrancescoPittaluga02@users.noreply.github.com>
2022-03-15 21:25:36 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-//Qui vanno definite le variabili
+int main() {
 
-cin >> a >> b >> c;
+    float a = 0 , b = 0 , c = 0 , area_triangolo = 0 , area_quadrato = 0 , area_rettangolo = 0 , area_trapezio = 0 ;
 
-//Qui va scritto il codice per eseguire i calcoli richiesti
+    cin >> a >> b >> c ;
 
+    area_triangolo = (a * b)/2 ;
+    area_quadrato = a * a  ;
+    area_rettangolo = a * b  ;
+    area_trapezio = ((a + b) * c)/2 ;
 
+    return 0;
 }
